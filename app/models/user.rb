59c7efb75467bab335ed934832d5c435c987ff7c
eric <eholdener12@gmail.com>
2022-07-29 12:47:53 -0700

    created routes for users and corresponding logic in users_controller.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true
+  validates :email, uniqueness: true
+
+  validates :first_name, presence: true
+  validates :first_name, uniqueness: true
+
+  validates :last_name, presence: true
+  validates :last_name, uniqueness: true
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :products
   resources :orders
   resources :discounts
+
+  resources :users, only: [:create, :show, :index]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
