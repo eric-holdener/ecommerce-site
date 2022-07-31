e94f3852b39d2f47fcaa4f0c8071d035819bdee0
EHoldener <eholdener12@gmail.com>
2022-07-31 09:00:51 -0700

    wrote routes for product queries, as well as controller functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/products/category/:category', to: 'products#show_category'
+  get '/products/product/:id', to: 'products#show_product'
 
   # Defines the root path route ("/")
   root 'pages#index'
