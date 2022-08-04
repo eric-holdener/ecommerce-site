71710675ce7420beee472e3749548e4c68695da8
EHoldener <eholdener12@gmail.com>
2022-08-03 18:29:36 -0700

    added ratings to individual product pages

--- app/javascript/components/components/Products/ProductDetails.js
+++ app/javascript/components/components/Products/ProductDetails.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { addToCart } from "../../redux/cart-slice";
 import React from 'react';
+import ProductRating from "./ProductRating";
 
 export default function ProductDetails() {
   const [data, setData] = useState(null);
@@ -26,18 +27,25 @@ export default function ProductDetails() {
     <div>
       {data ? (
         <div className="productPage">
+          {console.log(data)}
           <div className="productPageLeft">
             <img src={data.image} className="productImage"></img>
           </div>
           <div className="productPageRight">
-            <h1 className="productTitle">{data.title}</h1>
-            <p className="productDescription">{data.description}</p>
-            <p className="productPrice">${data.price}</p>
-            <input type="text" pattern="[0-9]*" value={quantity} onChange={(e) => handleChange(e.target.value)}/>
-            <button onClick={() => {
-                  dispatch(addToCart([data, quantity]))
-                }}>Add to Cart
-            </button>
+            <div className="productDetails">
+              <h1 className="productTitle">{data.title}</h1>
+              <p className="productDescription">{data.description}</p>
+              <p className="productPrice">${data.price}</p>
+              <input type="text" pattern="[0-9]*" value={quantity} onChange={(e) => handleChange(e.target.value)}/>
+              <button onClick={() => {
+                    dispatch(addToCart([data, quantity]))
+                  }}>Add to Cart
+              </button>
+            </div>
+            <div className="ratingsContainer">
+              <p className="ratingsHeader">Ratings</p>
+              {data.ratings.map((rating) => <ProductRating rating={rating} key={rating.id}/>)}
+            </div>
           </div>       
         </div>
       ) : (
