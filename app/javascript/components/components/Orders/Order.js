e9cc52d3adf2618fe51b9d2d9cb3b901a641c26e
EHoldener <eholdener12@gmail.com>
2022-08-03 20:36:28 -0700

    fixed order page

--- app/javascript/components/components/Orders/Order.js
+++ app/javascript/components/components/Orders/Order.js
@@ -2,22 +2,76 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { useRef } from 'react';
 
 export default function Order() {
   const [data, setData] = useState();
   const [isLoading, setIsLoading] = useState(true);
+  const [finalProducts, setFinalProducts] = useState({});
   const params = useParams();
   const [dom, setDom] = useState(null);
 
   useEffect(() => {
     fetch(`http://127.0.0.1:3000/api/v1/orders/order/${params.orderId}`)
       .then(res => res.json())
-      .then(json => setData(json))
+      .then(json => {
+        setData(json)
+        return json})
+      .then(data => {
+        newDataCreator(data);
+        return data;
+      })
+      .then(loading => setIsLoading(false))
   }, [params.orderId])
+
+  function newDataCreator(data) {
+    let intermediate = {};
+    data.products.map((product) => {
+      if (product.id in intermediate) {
+        intermediate[product.id].quantity += 1;
+      } else {
+        intermediate[product.id] = {
+          price: product.price,
+          quantity: 1,
+          title: product.title,
+          image: product.image,
+          id: product.id
+        };
+      };
+    })
+    setFinalProducts(intermediate);
+  }
+
+  function printProducts() {
+    return (
+      <>
+        {Object.entries(finalProducts).map(([key, product]) => {
+          return (
+            <Link to={`/product/${product.id}`} className='orderProductCard' id={`product-${product.id}`} state={{data: data}} key={product.id.id}>
+              <div className="orderProductCardLeft">
+                <img src={product.image} className="orderProductImage"></img>
+              </div>
+              <div className="orderProductCardRight">
+                <p className='orderProductTitle'>{product.title}</p>
+                <div style={{display: "flex"}}>
+                  <p>$</p>
+                  <p id={`productprice-${product.id}`} className='orderProductPrice'>{product.price}</p>
+                </div>
+                <div style={{display: "flex"}}>
+                  <p>Quantity:</p>
+                  <p id={`productquantity-${product.id}`} className='orderProductQuantity'>{product.quantity}</p>
+                </div>
+              </div>
+            </Link> 
+          )}
+        )}
+      </>
+    )
+  }
 
   return (
     <>
-      {data ? (
+      {!isLoading ? (
         <>
           <div className='orderInfoContainer'>
             <div><h1>Order #{params.orderId}</h1></div>
@@ -37,20 +91,7 @@ export default function Order() {
             </div>
           </div>
           <div className='orderProductsContainer'>
-            {data.products.map((product) => {
-              return (
-                <Link to={`/product/${product.id}`} className='orderProductCard' id={`product-${product.id}`} state={{data: data}}>
-                  <div className="orderProductCardLeft">
-                    <img src={product.image} className="orderProductImage"></img>
-                  </div>
-                  <div className="orderProductCardRight">
-                    <p className='orderProductTitle'>{product.title}</p>
-                    <p id={`productprice-${product.id}`} className='orderProductPrice'>${product.price}</p>
-                    <p id={`productquantity-${product.id}`} className='orderProductQuantity'>Quantity: 1</p>
-                  </div>
-                </Link>
-              )
-            })}
+            {printProducts()}
           </div>        
         </>
 
